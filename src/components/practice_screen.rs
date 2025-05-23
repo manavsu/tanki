d4488f27857c927befa7ea8740b23b24bfcb29ef
manav <manavjotsingh17@gmail.com>
2025-05-22 22:43:55 -0700

    added practice screen

--- src/components/practice_screen.rs
+++ src/components/practice_screen.rs
@@ -3,9 +3,16 @@ use crate::{
     models::{card::Card, deck::Deck},
 };
 use color_eyre::Result;
-use ratatui::{Frame, layout::Rect};
+use ratatui::{
+    Frame,
+    layout::{Constraint, Layout, Margin, Rect},
+    widgets::{Block, Borders, Paragraph},
+};
 
-#[derive(Default)]
+use super::title;
+
+const DIVIDER_TEXT: &str = "\n\n──────────\n\n";
+
 pub struct PracticeScreen {
     cnt: usize,
     cards: Vec<Card>,
@@ -18,9 +25,9 @@ enum Mode {
     Complete,
 }
 
-impl Default for Mode {
+impl Default for PracticeScreen {
     fn default() -> Self {
-        Mode::Front
+        Self { cnt: 0, cards: Vec::new(), mode: Mode::Front }
     }
 }
 
@@ -29,10 +36,23 @@ impl PracticeScreen {
         match action {
             Action::Screen(Screen::Practice(_)) => {
                 self.cnt = 0;
+                self.mode = Mode::Front;
                 self.cards = deck.get_cards();
             }
             Action::Char('c') => return Ok(Some(Action::Screen(Screen::Home))),
             Action::Char('q') => return Ok(Some(Action::Quit)),
+            Action::Space => match self.mode {
+                Mode::Front => self.mode = Mode::Back,
+                Mode::Back => {
+                    if self.cnt == self.cards.len() - 1 {
+                        self.mode = Mode::Complete;
+                    } else {
+                        self.cnt = self.cnt.wrapping_add(1) % self.cards.len();
+                        self.mode = Mode::Front;
+                    }
+                }
+                Mode::Complete => return Ok(Some(Action::Screen(Screen::Home))),
+            },
             _ => {}
         }
         Ok(None)
@@ -40,17 +60,34 @@ impl PracticeScreen {
 
     pub fn draw(&mut self, deck: &Deck, frame: &mut Frame, area: Rect) -> Result<()> {
         let card = self.cards[self.cnt].clone();
+        let chunks = Layout::vertical([Constraint::Length(7), Constraint::Min(0), Constraint::Length(3)]).split(area);
+        title::draw_title(frame, chunks[0])?;
         match self.mode {
-            Mode::Front => draw_front(card, frame, area),
-            Mode::Back => {}
-            Mode::Complete => {}
+            Mode::Front => self.draw_front(card, frame, chunks[1]),
+            Mode::Back => self.draw_back(card, frame, chunks[1]),
+            Mode::Complete => self.draw_complete(frame, chunks[1]),
         };
         Ok(())
     }
-}
 
-fn draw_status_bar(num_cards: usize, cnt: usize, frame: &mut Frame, area: Rect) {
-    let status = format!("Card {}/{}", cnt + 1, num_cards);
-}
+    fn draw_front(&self, card: Card, frame: &mut Frame, area: Rect) {
+        let front = Paragraph::new("\n".to_string() + &card.front + "\n")
+            .centered()
+            .block(Block::default().title(format!("[Practice][{}/{}]", self.cnt + 1, self.cards.len())).borders(Borders::ALL));
+        frame.render_widget(front, area);
+    }
 
-fn draw_front(card: Card, frame: &mut Frame, area: Rect) {}
+    fn draw_back(&self, card: Card, frame: &mut Frame<'_>, area: Rect) {
+        let front = Paragraph::new("\n".to_string() + &card.front + DIVIDER_TEXT + &card.back + "\n")
+            .centered()
+            .block(Block::default().title(format!("[Practice][{}/{}]", self.cnt + 1, self.cards.len())).borders(Borders::ALL));
+        frame.render_widget(front, area);
+    }
+
+    fn draw_complete(&self, frame: &mut Frame<'_>, area: Rect) {
+        frame.render_widget(
+            Paragraph::new("\nCompleted!\n").centered().block(Block::default().title("[Practice][Complete]").borders(Borders::ALL)),
+            area,
+        );
+    }
+}
